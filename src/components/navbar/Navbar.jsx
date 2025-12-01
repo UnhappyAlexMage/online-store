d963df5cc1396ac65d58dad2a488396746974d85
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-01 23:40:52 +0500

    Added component Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import styles from './navbar.module.scss';
+import basketPict from '../../assets/images/basket.png';
+
+export default function Navbar() {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleBurgerClick = evt => {
+        evt.preventDefault();
+        setShowMenu(!showMenu);
+    }
+
+    return(
+        <nav className={styles.navbar}>
+            <ul className={styles.container} onClick={handleBurgerClick}>
+                <li className={styles.li}>About</li>
+                <li className={styles.li}>Products</li>
+                <div className={styles.containerButtons}>
+                    <button className={styles.buttonLogin}>Sign In</button>
+                    <button className={styles.buttonRegis}>Sign Up</button>
+                </div>
+                <a className={styles.a} href="#"><img src={basketPict} alt="picture_shop" /></a>
+            </ul>
+            <a href="/" className={styles.burger}>
+                    <span className={styles.span}></span>
+                    <span className={styles.span}></span>
+                    <span className={styles.span}></span>
+                    <span className={styles.span}></span>
+            </a>
+        </nav>
+    );
+}
