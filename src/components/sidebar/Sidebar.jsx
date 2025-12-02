d68a08470b8a97b3acffd2f0fe7d4a2cff992538
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-02 23:58:15 +0500

    Added component Sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from './sidebar.module.scss';
+
+export default function Sidebar() {
+    return(
+        <aside className={styles.sidebar}>
+            <div className={styles.sidbarNavigation}>
+                <div className={styles.title}>
+                    <h2>CATEGORIES</h2>
+                </div>
+                <ul className={styles.sidebarList}>
+                    <li className={styles.sidebarItem}>Electronics</li>
+                    <li className={styles.sidebarItem}>Clothes</li>
+                    <li className={styles.sidebarItem}>Furniture</li>
+                    <li className={styles.sidebarItem}>Shoes</li>
+                    <li className={styles.sidebarItem}>Miscellaneous</li>
+                </ul>
+                <div className={styles.sidebarHelp}>
+                    <span><a href="">Help</a></span>
+                    <span><a href="">Terms & Conditions</a></span>
+                </div>
+            </div>
+        </aside>
+    );
+}
