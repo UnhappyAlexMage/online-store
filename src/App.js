128e7b390af3efd5590c5dde44dd6357ed0904af
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-04 23:36:03 +0500

    Change component App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header.jsx';
+
+import './styles/reset.scss';
+import './styles/global.scss';
+import Footer from './components/footer/Footer.jsx';
+import HomePage from './pages/home/HomePage.jsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
