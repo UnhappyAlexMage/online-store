f4b862ed2d1e4eecd5696cb12a04bf2a6e5c4315
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-06 23:20:32 +0500

    Added provider for store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import { Provider } from 'react-redux';
+import { store } from './store/store.js'; 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router.js';
@@ -9,7 +10,9 @@ import router from './routes/router.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
