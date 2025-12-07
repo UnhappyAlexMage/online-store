a947cc59c9c3f880d78611f6e57e114fd58abdf8
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-07 23:43:25 +0500

    Changed component HomePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,16 @@
-import Promo from '../../components/promo/Promo';
-import Sidebar from '../../components/sidebar/Sidebar';
+import Promo from '../../components/promo/Promo.jsx';
+import Sidebar from '../../components/sidebar/Sidebar.jsx';
+import GetProductsSection from '../../components/products/GetProductsSection.jsx';
 import styles from './homepage.module.scss';
 
 export default function HomePage() {
     return(
         <main className={styles.main}>
-            <Sidebar />
-            <Promo />
+            <div className={styles.blocks}>
+                <Sidebar />
+                <Promo />
+            </div>
+            <GetProductsSection />
         </main>
     );
 }
