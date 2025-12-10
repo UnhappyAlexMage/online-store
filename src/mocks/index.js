2222a7b07fad9a6f66d2df3d6a502b6313c69694
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-10 23:28:24 +0500

    Connection handlers

---
+++ src/mocks/index.js
@@ -0,0 +1,7 @@
+import { handlerProductsClothes } from "./handlers/handlerProductsClothes.js";
+import { handlerProductsMiscellaneous } from "./handlers/handlerProductsMiscellaneous.js";
+
+export const handlers = [
+    handlerProductsClothes,
+    handlerProductsMiscellaneous,
+];
