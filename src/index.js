e7e66f27581235b63f9395f74a7d4eba4d039785
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-10 23:25:35 +0500

    Added MockServiceWorker

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/router.js';
 
+async function enableMocking() {
+  if (process.env.NODE_ENV === "development") {
+    const { worker } = await import("./mocks/browser");
+
+    return worker.start();
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+enableMocking().then(() => {root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-);
+);})
 
 reportWebVitals();
