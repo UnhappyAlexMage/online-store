ee388e812dd2f49f7a718a038bda25636d70a909
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-16 23:28:30 +0500

    Changed component ProductsCartItem

--- src/components/ProductsCart/ProductsCartItem.jsx
+++ src/components/ProductsCart/ProductsCartItem.jsx
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import styles from './productscartitem.module.scss';
-import basketImg from '../../assets/images/basket.png';
 
-export default function ProductsCartItem() {
+export default function ProductsCartItem(props) {
+  const [count, setCount] = useState(1);
+
+  const handleClickMinus = () => {
+    if (count > 1) {
+      setCount(prevCount => prevCount - 1);
+    }
+  };
+
+  const handleClickPlus = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
     return (
         <div className={styles.block_productsItem}>
             <div className={styles.productsItem_info}>
-                <img className={styles.img} src={basketImg} alt="" />
+                <img className={styles.img} src={props.image} alt="" />
                 <div className={styles.block_names}>
-                    <p className={styles.name}>Nike ZoomX 2023</p>
-                    <span className={styles.category}>Sneakers</span>
+                    <p className={styles.name}>{props.title}</p>
+                    <span className={styles.category}>{props.category}</span>
                 </div>
             </div>
             <div className={styles.block_countProducts}>
-                <span className={styles.productPrice}>99$</span>
+                <span className={styles.productPrice}>{props.price}$</span>
                 <div className={styles.block_count}>
-                    <button className={styles.button_minus}>—</button>
-                    <span className={styles.count}>1</span>
-                    <button className={styles.button_plus}>+</button>
+                    <button className={styles.button_minus} onClick={handleClickMinus}>-</button>
+                    <span className={styles.count}>{count}</span>
+                    <button className={styles.button_plus} onClick={handleClickPlus}>+</button>
                 </div>
-                <span className={styles.productPriceCount}>99$</span>
+                <span className={styles.productPriceCount}>{count * props.price}$</span>
             </div>
             <a className={styles.burgerClose} href="#">
                 <span className={styles.span}></span>
