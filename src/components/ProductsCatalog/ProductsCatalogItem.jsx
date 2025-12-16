2095a08437b4b978e56c25bc26ac11ec00c1f67b
UnhappyAlex <alexandr.bro03@gmail.com>
2025-12-16 23:29:25 +0500

    Ghanged component ProductsCatalogItem

--- src/components/ProductsCatalog/ProductsCatalogItem.jsx
+++ src/components/ProductsCatalog/ProductsCatalogItem.jsx
@@ -1,14 +1,29 @@
-import styles from './productscatalogitem.module.scss';
-import basketLogo from '../../assets/images/basket.png';
+import { useDispatch } from 'react-redux';
+import { selectProduct } from '../../store/productsSlice.js';
 
-export default function ProductsCatalogItem() {
+import styles from './productscatalogitem.module.scss';
+
+export default function ProductsCatalogItem(props) {
+    const dispatch = useDispatch();
+
+    const dispatchProductModal = () => {
+        dispatch(selectProduct(props));
+    }
+
+    const handleClickProudctModal = () => {
+        if(props.onClick)
+            props.onClick();
+        dispatchProductModal();
+    }
+
     return(
-        <div className={styles.itemblock} onClick={1}>
-            <img className={styles.itemblock_img} src={basketLogo} alt="item-logo" />
+        <div className={styles.itemblock} onClick={handleClickProudctModal}>
+            <img className={styles.itemblock_img} src={props.image} alt="item-logo" />
             <div className={styles.itemblock_description}>
-                <h5 className={styles.descriptionh5}>Nike</h5>
-                <p className={styles.descriptionsp}>Nike super duper krossovki da da</p>
-                <span className={styles.descriptionspan}>99$</span>
+                <h5 className={styles.descriptionh5}>{props.title}</h5>
+                <p className={styles.descriptionsp}>{props.slug}</p>
+                <p className={styles.category}>{props.category}</p>
+                <span className={styles.descriptionspan}>{props.price}$</span>
             </div>
         </div>
     );
